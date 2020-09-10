d0d1b841570a1f2235518daf1036fbf19ddf4798
Mykhailo Kuznietsov <mkuznets@redhat.com>
2020-09-10 10:11:28 +0300

    continue release 7.18.2

--- cico_release.sh
+++ cico_release.sh
@@ -566,11 +566,11 @@ evaluateCheVariables
 set -e
 
 #release che-theia, machine-exec and devfile-registry
- { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-theia            devtools-che-theia-che-release        90 & }; pid_1=$!;
- { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-machine-exec     devtools-che-machine-exec-release     60 & }; pid_2=$!;
- { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-devfile-registry devtools-che-devfile-registry-release 75 & }; pid_3=$!;
-waitForPids $pid_1 $pid_2 $pid_3
-wait
+#  { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-theia            devtools-che-theia-che-release        90 & }; pid_1=$!;
+#  { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-machine-exec     devtools-che-machine-exec-release     60 & }; pid_2=$!;
+#  { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-devfile-registry devtools-che-devfile-registry-release 75 & }; pid_3=$!;
+# waitForPids $pid_1 $pid_2 $pid_3
+# wait
 #then release plugin-registry (depends on che-theia and machine-exec)
 
 verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-machine-exec:${CHE_VERSION} 5

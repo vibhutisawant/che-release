54cb0da8cdf402441f787d9eb813c7e4b508dd50
nickboldt <nboldt@redhat.com>
2021-03-19 13:04:11 -0400

    add dwo into phase 1 (to match new readme doc); add missing verifyContainerExistsWithTimeout checks for DWO and DWCO; fix phase list for when to check che images; ressurect missing IMAGES_LIST array (why was it removed but the for loop on which it depends remains?); merge cico_common into cico_release (why have a common file when we're not sharing it?); rename to make-release.sh for consistency w/ other projects (and because centos CI is no longer in the mix)
    Change-Id: I6e7a87125b283a686ac708ae7a181d988c0af553
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -1,10 +1,92 @@
 #!/bin/bash
 
-#include common scripts
-. ./cico_common.sh
+# overall Che release orchestration script
+# see ../README.md for more info
 
+SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 REGISTRY="quay.io"
 ORGANIZATION="eclipse"
+
+function die_with() 
+{
+	echo "$*" >&2
+	exit 1
+}
+
+verifyContainerExistsWithTimeout()
+{
+    this_containerURL=$1
+    this_timeout=$2
+    containerExists=0
+    count=1
+    (( timeout_intervals=this_timeout*3 ))
+    while [[ $count -le $timeout_intervals ]]; do # echo $count
+        sleep 20s
+        echo "       [$count/$timeout_intervals] Verify ${1} exists..." 
+        # check if the container exists
+        verifyContainerExists "$1"
+        if [[ ${containerExists} -eq 1 ]]; then break; fi
+        (( count=count+1 ))
+    done
+    # or report an error
+    if [[ ${containerExists} -eq 0 ]]; then
+        echo "[ERROR] Did not find ${1} after ${this_timeout} minutes - script must exit!"
+        exit 1;
+    fi
+}
+
+# for a given container URL, check if it exists and its digest can be read
+# verifyContainerExists quay.io/crw/pluginregistry-rhel8:2.6 # schemaVersion = 1, look for tag
+# verifyContainerExists quay.io/eclipse/che-plugin-registry:7.24.2 # schemaVersion = 2, look for arches
+verifyContainerExists()
+{
+    this_containerURL="${1}"
+    this_image=""; this_tag=""
+    this_image=${this_containerURL#*/}
+    this_tag=${this_image##*:}
+    this_image=${this_image%%:*}
+    this_url="https://quay.io/v2/${this_image}/manifests/${this_tag}"
+    # echo $this_url
+
+    # get result=tag if tag found, result="null" if not
+    result="$(curl -sSL "${this_url}"  -H "Accept: application/vnd.docker.distribution.manifest.list.v2+json" 2>&1 || true)"
+    if [[ $(echo "$result" | jq -r '.schemaVersion' || true) == "1" ]] && [[ $(echo "$result" | jq -r '.tag' || true) == "$this_tag" ]]; then
+        echo "[INFO] Found ${this_containerURL} (tag = $this_tag)"
+        containerExists=1
+    elif [[ $(echo "$result" | jq -r '.schemaVersion' || true) == "2" ]]; then
+        arches=$(echo "$result" | jq -r '.manifests[].platform.architecture')
+        if [[ $arches ]]; then
+            echo "[INFO] Found ${this_containerURL} (arches = "$arches")"
+        fi
+        containerExists=1
+    else
+        # echo "[INFO] Did not find ${this_containerURL}"
+        containerExists=0
+    fi
+}
+
+# TODO make this work, now that we're on newer Bash
+waitForPids() {
+    pstree -p | grep -v grep > out
+    cat out
+
+    procs=$(pstree -p | grep -v grep | grep -E "cico_release_theia_and_registries|cico_release_dashboard_and_workspace_loader" | sed -r -e "s#--# #g")
+    for p in $procs; do p=$(echo "${p}" | tr "|" -d | sed -r -e "s#.*\(([0-9]+)\).*#\1#g" | tr -d "-"); procslist="${procslist} ${p}"; done
+    echo "JOB PIDs RUNNING:
+     ----------
+     $(jobs -rl)
+     $procs
+     ${procslist}
+     ----------
+     "
+     
+    echo "$*"
+     wait $* || {
+         echo  "Exit due to failure; kill running processes"
+         trap "kill ${procslist} 2>/dev/null" EXIT
+     exit 1
+    }
+}
 
 installRPMDeps(){
     set +x
@@ -132,6 +214,7 @@ releaseOperator() {
 releaseDwoOperator() {
     invokeAction devfile/devworkspace-operator "Release DevWorkspace Operator" "6380164" version "${DWO_VERSION}"
 }
+
 releaseDwoCheOperator() {
     invokeAction che-incubator/devworkspace-che-operator "Release DevWorkspace Che Operator" "6597719" version "v${CHE_VERSION}"
 }
@@ -173,36 +256,65 @@ if [[ ${PHASES} == *"1"* ]]; then
     releaseMachineExec
     releaseCheTheia
     releaseDevfileRegistry
+
     releaseDashboardAndWorkspaceLoader
+    releaseDwoOperator
     branchJWTProxyAndKIP
 fi
 wait
 verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-machine-exec:${CHE_VERSION} 30
-verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-devfile-registry:${CHE_VERSION} 30
 verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-theia-dev:${CHE_VERSION} 30
 verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-theia:${CHE_VERSION} 30
 verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-theia-endpoint-runtime-binary:${CHE_VERSION} 30
+verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-devfile-registry:${CHE_VERSION} 30
+
 verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-dashboard:${CHE_VERSION} 30
 verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-workspace-loader:${CHE_VERSION} 30
+
+# https://quay.io/repository/devfile/devworkspace-controller?tab=tags
+verifyContainerExistsWithTimeout ${REGISTRY}/devfile/devworkspace-controller:${DWO_VERSION} 30
 
 # Release plugin-registry (depends on che-theia and machine-exec)
 set +x
 if [[ ${PHASES} == *"2"* ]]; then
     releasePluginRegistry
 fi
+
+# Release server (depends on dashboard)
 if [[ ${PHASES} == *"3"* ]]; then
     releaseCheServer
 fi
+
+# Release devworkspace che operator 
+# TODO this will go away when it's part of che-operator
 if [[ ${PHASES} == *"4"* ]]; then
     releaseDwoCheOperator
 fi
 wait
+
+# TODO this will go away when it's part of che-operator
+if [[ ${PHASES} == *"4"* ]] || [[ ${PHASES} == *"5"* ]]; then
+    # https://quay.io/repository/che-incubator/devworkspace-che-operator?tab=tags
+    verifyContainerExistsWithTimeout ${REGISTRY}/che-incubator/devworkspace-che-operator:${CHE_VERSION} 30
+fi
+
 if [[ ${PHASES} == *"2"* ]] || [[ ${PHASES} == *"3"* ]] || [[ ${PHASES} == *"4"* ]] || [[ ${PHASES} == *"5"* ]]; then
   verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-plugin-registry:${CHE_VERSION} 30
 fi
-if [[ ${PHASES} == *"3"* ]] || [[ ${PHASES} == *"5"* ]]; then
+
+IMAGES_LIST=(
+    quay.io/eclipse/che-endpoint-watcher
+    quay.io/eclipse/che-keycloak
+    quay.io/eclipse/che-postgres
+    quay.io/eclipse/che-dev
+    quay.io/eclipse/che-server
+    quay.io/eclipse/che-dashboard-dev
+    quay.io/eclipse/che-e2e
+)
+
+if [[ ${PHASES} == *"3"* ]] || [[ ${PHASES} == *"4"* ]] || [[ ${PHASES} == *"5"* ]]; then
     # verify images all created from IMAGES_LIST
-    for image in ${IMAGES_LIST[@]}; do
+    for image in "${IMAGES_LIST[@]}"; do
         verifyContainerExistsWithTimeout ${image}:${CHE_VERSION} 60
     done
 fi
@@ -214,4 +326,5 @@ if [[ ${PHASES} == *"5"* ]]; then
 fi
 wait
 
-# TODO update to list remaining manual steps (PR merges only) https://github.com/eclipse/che-release/blob/master/README.md#phase-2---manual-steps
+# downstream steps depends on Che operator PRs being merged by humans, so this is the end of the automation.
+# see ../README.md for more info
